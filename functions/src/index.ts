de7a7300cfe672faa6e4cb757e57c90946151d58
enta0701 <endo.takuya.0701@gmail.com>
2018-03-14 23:16:19 +0900

    init

---
+++ functions/src/index.ts
@@ -0,0 +1,15 @@
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as basicAuth from 'basic-auth-connect';
+
+const app = express();
+
+// Basic Authfication
+app.use(basicAuth('hoge', 'fuga'));
+
+// Redirect root
+app.get('/auth', (req, res) => {
+  res.redirect('/index.html')
+})
+
+exports.app = functions.https.onRequest(app)
